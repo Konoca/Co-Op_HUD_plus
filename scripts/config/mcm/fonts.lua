11c6298e53e6cf0d2f64cfbc4566841a43097326
Konoca <ichris170@gmail.com>
2024-05-25 18:43:26 -0400

    add font warning, fix extra lives text

--- scripts/config/mcm/fonts.lua
+++ scripts/config/mcm/fonts.lua
@@ -2,6 +2,7 @@ local mod = CoopHUDplus
 local Utils = mod.Utils
 
 -- TODO every time you change fonts, the memory used goes up and doesn't go back down
+-- For now, warning is given
 
 local fonts = {}
 for key, _ in pairs(mod.PATHS.FONTS) do
@@ -28,6 +29,7 @@ ModConfigMenu.AddSetting(
         CurrentSetting = function() return getTableIndex(fonts, mod.config.fonts.timer) end,
         Display = function() return 'Timer: ' .. mod.config.fonts.timer end,
         OnChange = function(n) mod.config.fonts.timer = fonts[n] Utils.LoadFonts() end,
+        Info = {'Warning! Game might lag after changing fonts, please restart your game after changing this setting.'}
     }
 )
 ModConfigMenu.AddSetting(
@@ -40,6 +42,7 @@ ModConfigMenu.AddSetting(
         CurrentSetting = function() return getTableIndex(fonts, mod.config.fonts.streaks) end,
         Display = function() return 'Streaks: ' .. mod.config.fonts.streaks end,
         OnChange = function(n) mod.config.fonts.streaks = fonts[n] Utils.LoadFonts() end,
+        Info = {'Warning! Game might lag after changing fonts, please restart your game after changing this setting.'}
     }
 )
 ModConfigMenu.AddSetting(
@@ -52,6 +55,7 @@ ModConfigMenu.AddSetting(
         CurrentSetting = function() return getTableIndex(fonts, mod.config.fonts.pickups) end,
         Display = function() return 'Pickups: ' .. mod.config.fonts.pickups end,
         OnChange = function(n) mod.config.fonts.pickups = fonts[n] Utils.LoadFonts() end,
+        Info = {'Warning! Game might lag after changing fonts, please restart your game after changing this setting.'}
     }
 )
 ModConfigMenu.AddSetting(
@@ -64,6 +68,7 @@ ModConfigMenu.AddSetting(
         CurrentSetting = function() return getTableIndex(fonts, mod.config.fonts.stats) end,
         Display = function() return 'Stats: ' .. mod.config.fonts.stats end,
         OnChange = function(n) mod.config.fonts.stats = fonts[n] Utils.LoadFonts() end,
+        Info = {'Warning! Game might lag after changing fonts, please restart your game after changing this setting.'}
     }
 )
 ModConfigMenu.AddSetting(
@@ -76,6 +81,7 @@ ModConfigMenu.AddSetting(
         CurrentSetting = function() return getTableIndex(fonts, mod.config.fonts.pocket_items) end,
         Display = function() return 'Pocket Items: ' .. mod.config.fonts.pocket_items end,
         OnChange = function(n) mod.config.fonts.pocket_items = fonts[n] Utils.LoadFonts() end,
+        Info = {'Warning! Game might lag after changing fonts, please restart your game after changing this setting.'}
     }
 )
 ModConfigMenu.AddSetting(
@@ -88,5 +94,6 @@ ModConfigMenu.AddSetting(
         CurrentSetting = function() return getTableIndex(fonts, mod.config.fonts.extra_lives) end,
         Display = function() return 'Extra Lives: ' .. mod.config.fonts.extra_lives end,
         OnChange = function(n) mod.config.fonts.extra_lives = fonts[n] Utils.LoadFonts() end,
+        Info = {'Warning! Game might lag after changing fonts, please restart your game after changing this setting.'}
     }
 )
