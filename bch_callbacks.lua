4ec63f9f6fcba30098a6e035337cecbf5c74592e
Konoca <56772772+Konoca@users.noreply.github.com>
2024-08-08 16:07:02 -0400

    comment out unused callbacks

--- bch_callbacks.lua
+++ bch_callbacks.lua
@@ -17,19 +17,19 @@ end
 Better_Coop_HUD:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, onGameStart)
 
 
-local function onPlayerInit(_, player)
-    local p = Better_Coop_HUD.Player.new(player)
-    Better_Coop_HUD.players[p.player_entity.Index] = p
-end
+-- local function onPlayerInit(_, player)
+--     local p = Better_Coop_HUD.Player.new(player)
+--     Better_Coop_HUD.players[p.player_entity.Index] = p
+-- end
 -- Better_Coop_HUD:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, onPlayerInit)
 
 
-local function onPlayerUpdate(_, player)
-    local idx = player.Index
-    local p = Better_Coop_HUD.players[idx]
-    if p then p:update(player) end
-    if not p then onPlayerInit(_, player) end
-end
+-- local function onPlayerUpdate(_, player)
+--     local idx = player.Index
+--     local p = Better_Coop_HUD.players[idx]
+--     if p then p:update(player) end
+--     if not p then onPlayerInit(_, player) end
+-- end
 -- Better_Coop_HUD:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, onPlayerUpdate)
 -- Better_Coop_HUD:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, onPlayerUpdate)
 
